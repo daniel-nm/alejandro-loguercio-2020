c8c68ea9eee5c1978835b1282acf7dedd6603bf2
Daniel <daniel.naranjo.merino@gmail.com>
2021-01-18 08:28:50 +0100

    Added ScrollToTop function so page goes to top when route change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, useLocation, withRouter } from "react-router-dom";
+
+function ScrollToTop(props) {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log("timeout 1");
+    setTimeout(() => {
+      console.log("timeout 2");
+      window.scrollTo(0, 0);
+    }, 1000);
+  }, [pathname]);
+
+  return props.children;
+}
+
+const ScrollTop = withRouter(ScrollToTop);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollTop>
+        <App />
+      </ScrollTop>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
