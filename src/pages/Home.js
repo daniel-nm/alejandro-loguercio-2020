ea8f46e90eae33f756108c910c8980694261f2f8
Daniel <daniel.naranjo.merino@gmail.com>
2021-01-21 08:46:51 +0100

    Removed Classes section (no longer required)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { motion } from "framer-motion";
 
 // Context
@@ -11,31 +11,18 @@ import Content from "../content/home.json";
 import HomeBanner from "../components/home/HomeBanner";
 import HomePress from "../components/home/HomePress";
 import HomeProjects from "../components/home/HomeProjects";
-import HomeClasses from "../components/home/HomeClasses";
 import HomeContact from "../components/home/HomeContact";
 import PanelTransition from "../components/panel/PanelTransition";
 
 const Home = () => {
 
   const [language] = useContext(LanguageContext);
-  const [isGerman, setIsGerman] = useState(false);
-
-  // Show the class section only for German language
-  useEffect(() => {
-    if(language === "de") {
-      setIsGerman(true);
-    } 
-    else {
-      setIsGerman(false);
-    }
-  }, [language]);
 
   return (
     <motion.div exit="exit">
       <HomeBanner content={Content} language={language} />
       <HomePress content={Content} language={language} />
       <HomeProjects content={Content} language={language} />
-      {isGerman && <HomeClasses content={Content} language={language} />}
       <HomeContact content={Content} language={language} />
       <PanelTransition />
     </motion.div>
