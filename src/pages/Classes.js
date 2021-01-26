4966e4129b9381636c337a695bea4c37fe064953
Daniel <daniel.naranjo.merino@gmail.com>
2021-01-26 12:58:42 +0100

    Completed Classes page

---
+++ src/pages/Classes.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useContext } from 'react';
+
+// Components 
+import ClassText from "../components/classes/ClassText";
+import ClassBio from "../components/classes/ClassBio";
+import ClassVideo from "../components/classes/ClassVideo";
+import ClassPrices from "../components/classes/ClassPrices";
+import ClassContact from '../components/classes/ClassContact';
+import PanelTransition from "../components/panel/PanelTransition";
+
+// Styles
+import { Page } from "../styles/components/classesStyles";
+
+// Animation
+import { transition } from "../animation/biography";
+
+// Content
+import Content from "../content/classes.json";
+
+// Context
+import LanguageContext from "../context/LanguageContext";
+
+const Classes = () => {
+
+  // App language
+  const [language, setLanguage] = useContext(LanguageContext);
+
+  // Set language to German
+  useEffect(() => {
+    localStorage.setItem("AL - Lang", "de");
+    setLanguage("de");
+  }, [setLanguage]);
+
+  return (
+    <Page>
+      <ClassText content={Content} language={language} transition={transition} />
+      <ClassBio content={Content} language={language} />
+      <ClassVideo />
+      <ClassPrices content={Content} language={language} />
+      <ClassContact content={Content} language={language} />
+      <PanelTransition />
+    </Page>
+  )
+}
+
+export default Classes
